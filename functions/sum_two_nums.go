c4475d2ae80e3f7f411a14ea7f405f6e097710c3
Gib Iglesias <goku.simpson.9517@gmail.com>
2025-07-24 15:57:28 -0600

    changes in repo

---
+++ functions/sum_two_nums.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b int
+	fmt.Print("Ingresa el primer número: ")
+	fmt.Scan(&a)
+	fmt.Print("Ingresa el segundo número: ")
+	fmt.Scan(&b)
+	fmt.Printf("La suma es: %d\n", a+b)
+}
